b3e35ad002a7617c98c34979dc197b4486c1c7f9
animeAVACADO <99669778+animeAVACADO@users.noreply.github.com>
2022-02-19 16:19:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function add() {
+    var result =Number(document.getElementById("firstno").value) +Number(document.getElementById("secondno").value);
+    console.log(result);
+     window.alert(result); 
+     
+     }
+    
+    function sub() {
+    var result1 =Number(document.getElementById("firstno").value)  -Number(document.getElementById("secondno").value);
+    console.log(result1);
+     window.alert(result1); 
+     
+     }
+    
+    function mul() {
+    var result2 =Number(document.getElementById("firstno").value) *Number(document.getElementById("secondno").value);
+    console.log(result2);
+     window.alert(result2); 
+     
+     }
+    
+    function div() {
+    var result3 =Number(document.getElementById("firstno").value) /Number(document.getElementById("secondno").value);
+    console.log(result3);
+     window.alert(result3); 
+     
+     }
